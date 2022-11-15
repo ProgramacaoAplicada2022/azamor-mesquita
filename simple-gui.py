1f2f8e4e96698b34fdb0f6beefe16cc2d9f2d0a9
Luccas Azamor <luccas.azamor@gmail.com>
2022-11-15 20:50:52 -0300

    simplified gui

--- simple-gui.py
+++ simple-gui.py
@@ -149,26 +149,24 @@ class Application(Funcs, Relatorios):
         self.tela()
         self.frames_da_tela()
         self.widgets_frame1()
-        self.lista_frame2()
-        self.montaTabelas()
-        self.select_lista()
-        self.Menus()
+        #self.lista_frame2()
+        #self.montaTabelas()
+        #self.select_lista()
+        #self.Menus()
         root.mainloop()
     def tela(self):
         self.root.title("Cadastro de Clientes")
         self.root.configure(background= '#1e3743')
-        self.root.geometry("700x500")
+        self.root.geometry("700x250")
         self.root.resizable(True, True)
         self.root.maxsize(width= 900, height= 700)
         self.root.minsize(width=500, height= 400)
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root, bd = 4, bg= '#dfe3ee',
                              highlightbackground= '#759fe6', highlightthickness=3 )
-        self.frame_1.place(relx= 0.02, rely=0.02, relwidth= 0.96, relheight= 0.46)
+        self.frame_1.place(relx= 0.02, rely=0.02, relwidth= 0.96, relheight= 0.95)
 
-        self.frame_2 = Frame(self.root, bd=4, bg='#dfe3ee',
-                             highlightbackground='#759fe6', highlightthickness=3)
-        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
+        
     def widgets_frame1(self):
         self.canvas_bt = Canvas(self.frame_1, bd=0, bg='#1e3743', highlightbackground = 'gray',
             highlightthickness=5)
