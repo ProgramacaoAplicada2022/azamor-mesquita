d3b13429c476c3bbf275692345d248286093d2ce
Luccas Azamor <luccas.azamor@gmail.com>
2022-11-16 18:18:40 -0300

    fix: remove unused code

--- bubbleDriver.py
+++ bubbleDriver.py
@@ -1,6 +1,5 @@
 #python3.7
 import undetected_chromedriver as uc
-#By = uc.selenium.webdriver.common.by.By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
